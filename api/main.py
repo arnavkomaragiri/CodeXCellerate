2c2d0006cdacf19083205fbd0ec75a70333fb857
arnavkomaragiri <arnav.komaragiri@gmail.com>
2021-09-18 17:37:46 -0400

    Update main.py
    Added request body and better runtime optimization

--- api/main.py
+++ api/main.py
@@ -6,14 +6,20 @@ from typing import Optional
 from fastapi import FastAPI
 from io import StringIO
 from contextlib import redirect_stdout
+from pydantic import BaseModel
 
 app = FastAPI()
 
 engine = "davinci-codex"
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = open('env.txt').readlines()[0]
+
+# data model for the request body
+class Item(BaseModel):
+    code: str
+    
 
 @app.get("/debug/completion")
-def completion(text: str, max_tokens: Optional[int]=50, stop: Optional[str]=None):
+def completion(text: str, max_tokens: Optional[int]=None, stop: Optional[str]=None):
     return openai.Completion.create(
         engine=engine,
         text=text,
@@ -25,13 +31,20 @@ def completion(text: str, max_tokens: Optional[int]=50, stop: Optional[str]=None
     )
 
 @app.get("/runtime")
-def runtime(code: str, lang: str, max_tokens: Optional[int]=50, stop: Optional[str]=None):
+def runtime(code: Item, lang: str, max_tokens: Optional[int]=None, stop: Optional[str]=None):
     if lang.lower() == "python":
         prefix = "#"
     elif lang.lower() == "c" or lang.lower() == "c++" or lang.lower() == "javascript":
         prefix = "//"
-    prompt = " Optimize the runtime of the above code\n"
-    return completion(prefix + prompt + code, max_tokens, stop)
+    prompts = [" Optimize the runtime of the above code", " Optimize the runtime complexity of the above code", 
+                " Optimize the time complexity of the above code", " Improve the runtime complexity of the above code",
+                " Improve the runtime of the above code", " Improve the time complexity of the above code"]
+    results = [completion(code.code + "\n" + prefix + prompt + "\n", max_tokens, stop) for prompt in prompts]
+    programs = [result["choices"][0]["text"] for result in results]
+    char_freq_map = lambda x: {c: x.count(c) for c in x}
+    char_freq_maps = list(set([(i, char_freq_map(program)) for i, program in enumerate(programs)]))
+    return [programs[i] for i, _ in char_freq_maps] 
+
 
 @app.get("/explain")
 def explain(code: str, max_tokens: Optional[int]=50, stop: Optional[str]=None):
