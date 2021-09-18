b90d62369416089be1b568fe96c38dbb3486cce7
arnavkomaragiri <arnav.komaragiri@gmail.com>
2021-09-18 12:53:13 -0400

    Update app.py
    Added basic OpenAI integration

--- api/app.py
+++ api/app.py
@@ -1,10 +1,25 @@
-from typing import Optional
+import os
+import openai
 
+from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()
 
 engine = "cushman-codex"
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+@app.get("/debug/completion")
+def completion(text: str, max_tokens: Optional[int]=50, stop: Optional[str]=None):
+    return openai.Completion.create(
+        engine=engine,
+        text=text,
+        max_tokens=max_tokens,
+        temperature=0,
+        frequency_penalty=0.0, # TODO: Tune these
+        presence_penalty=0.0,
+        stop=stop
+    )
 
 @app.get("/")
 def read_root():
