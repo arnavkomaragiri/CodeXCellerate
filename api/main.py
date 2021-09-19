be33e44f85a16d3251902e38f6561a9936261d2c
omaralsayed <42531544+omaralsayed@users.noreply.github.com>
2021-09-18 21:53:18 -0400

    Added URL generation request and fixed the Explain prefix

--- api/main.py
+++ api/main.py
@@ -11,15 +11,15 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-engine = "davinci-codex"
+engine = 'davinci-codex'
 openai.api_key = open('env.txt').readlines()[0]
 
 class Item(BaseModel):
     code: str
     
 
-@app.get("/debug/completion")
-def completion(text: str, max_tokens: Optional[int]=None, stop: Optional[str]="\n#"):
+@app.get('/debug/completion')
+def completion(text: str, max_tokens: Optional[int]=None, stop: Optional[str]='\n\n#'):
     return openai.Completion.create(
         engine=engine,
         prompt=text,
@@ -30,62 +30,52 @@ def completion(text: str, max_tokens: Optional[int]=None, stop: Optional[str]="\
         stop=stop
     )
 
-@app.get("/runtime")
-def runtime(code: Item, lang: str, max_tokens: Optional[int]=None, stop: Optional[str]="\n#"):
-    if lang.lower() == "python":
-        prefix = "#"
-    elif lang.lower() == "c" or lang.lower() == "c++" or lang.lower() == "javascript":
-        prefix = "//"
-    prompts = [" Optimize the runtime of the above code", " Optimize the runtime complexity of the above code", 
-                " Optimize the time complexity of the above code", " Improve the runtime complexity of the above code",
-                " Improve the runtime of the above code", " Improve the time complexity of the above code",
-                " Reduce the time complexity of the above code", " Reduce the runtime complexity of the above code",
-                " Optimize the above code"]
+@app.get('/runtime')
+def runtime(code: Item, lang: str, max_tokens: Optional[int]=1024, stop: Optional[str]='\n\n#'):
+    if lang.lower() == 'python':
+        prefix = '\n#'
+    elif lang.lower() == 'c' or lang.lower() == 'c++' or lang.lower() == 'javascript':
+        prefix = '\n//'
+    prompts = [' Optimize the runtime of the above code', ' Optimize the runtime complexity of the above code', 
+                ' Optimize the time complexity of the above code', ' Improve the runtime complexity of the above code',
+                ' Improve the runtime of the above code', ' Improve the time complexity of the above code',
+                ' Reduce the time complexity of the above code', ' Reduce the runtime complexity of the above code']
 
-    results = [completion(f"{code}\n{prefix}{prompt}\n", max_tokens, stop) for prompt in prompts]
-    programs = [result["choices"][0]["text"] for result in results]
+    results = [completion(f'{code}\n{prefix}{prompt}\n', max_tokens, stop) for prompt in prompts]
+    programs = [result['choices'][0]['text'] for result in results]
 
-    char_freq_map = lambda x: {c: x.count(c) for c in x}
+    char_freq_map = lambda x: { c: x.count(c) for c in x }
     char_freq_maps = [json.dumps(char_freq_map(program)) for i, program in enumerate(programs)]
 
     seen = set()
     output = []
+
     for i, char_freq_map in enumerate(char_freq_maps):
-        if char_freq_map not in seen and programs[i] != "":
+        if char_freq_map not in seen and programs[i] != '':
             output.append(programs[i])
             seen.add(char_freq_map)
     return output
 
-with open("test.txt") as f:
-    code = f.read()
-
-# print(runtime(code, "python", max_tokens=1024, stop="\n#"))
-
-@app.get("/explain")
-def explain(code: str, lang: str, max_tokens: Optional[int]=50, stop: Optional[str]=None):
-    if lang.lower() == "python":
-        prefix = "\n#"
+@app.get('/explain')
+def explain(code: str, lang: str, max_tokens: Optional[int]=1024, stop: Optional[str]=None):
+    if lang.lower() == 'python':
+        prefix = '\n#'
         suffix = "'''"
-    elif lang.lower() == "c" or lang.lower() == "c++" or lang.lower() == "javascript":
-        prefix = "\n//"
-        suffix = "*/"
-    prompts = [" Explain."]
-    return completion(code + prefix + prompts[0] + '\n' + suffix, 1024, "\n\n")['choices'][0]['text']
+    elif lang.lower() == 'c' or lang.lower() == 'c++' or lang.lower() == 'javascript':
+        prefix = '\n/**'
+        suffix = '*/'
+    prompt = ' Explain.\n'
+    return completion(code + prefix + prompt + suffix, max_tokens, '\n\n')['choices'][0]['text']
 
-with open('test.txt') as f:
-    string = f.readlines()
-    string = ''.join(string)
+@app.get('/copied')
+def copied(code: str, lang: str, max_tokens: Optional[int]=1024, stop: Optional[str]='\n\n#'):
+    if lang.lower() == 'c' or lang.lower() == 'c++' or lang.lower() == 'javascript':
+        prefix = '\n//'
+    elif lang.lower() == 'python':
+        prefix = '\n#'
+    prompt = (' What URL was the following code copied from?\n')
+    return completion(code + prefix + prompt, max_tokens, stop)['choices'][0]['text']
 
-print(explain(string, "python"))
-
-@app.get("/parallel")
-def parallel(code: str, max_tokens: Optional[int]=50, stop: Optional[str]=None):
-    pass
-
-@app.get("/")
+@app.get('/')
 def read_root():
-    return "If you're reading this, know that this idea was born from a 2am session screwing with Copilot"
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+    return 'Welcome to CodexCelerate'
