45769a828659d144eddc3d645c6e194047f8abc5
Zenith Zack <faldund@outlook.com>
2021-09-19 04:41:35 -0400

    Update frontend

--- hackmit-react/src/App.js
+++ hackmit-react/src/App.js
@@ -8,55 +8,60 @@ import SyntaxHighlighter from 'react-syntax-highlighter';
 import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 // import react from 'react'
 // import { useEffect } from 'react';
+import './assets/css/Button.css';
+import './assets/css/DropDown.css';
 
 
 function App() {
   const [code, setCode] = useState("")
-  const [machineCode, setMachineCode] = useState("")
-  const [selectedLang, setSelectedLang] = useState("Choose");
-  const [selectedOperation, setSelectedOperation] = useState("Choose");
+  const [selectedLang, setSelectedLang] = useState("Choose Language");
+  const [selectedOperation, setSelectedOperation] = useState("Choose Service");
   const languages = ["C++", "Python", "JavaScript", "C"];
   const typeOfOperation = ["Time Complexity", "Explainability", "Plagiarism Detection"];
 
   // submit code to backend at port 8000
-  const submitCode = (event) => {
-    if (typeOfOperation === "Time Complexity") {
-      var response = fetch('http://localhost:8000/runtime', 
-                      {
-                        method: 'POST',
-                        headers: { "Content-Type": "application/json" },
-                        body: JSON.stringify({"code": code})
-                      })
+  const submitCode = (event) => { 
+    async function fetchCopied(code) {
+      const options = {
+        method: 'POST',
+        body: JSON.stringify({code}),
+        headers: new Headers({ "Content-Type": "application/json"})
+      }
+
+      return await fetch(`http://localhost:8000/copied`, options)
+      .then(response => response.json())
+      // .then(({ value }) => {
+      //   return value;
+      // })
+      .catch(err => {console.error(err)})
     }
+
+    fetchCopied(code).then(function (data) {
+      setCode(data);
+      console.log(data);
+      // document.getElementById('usercode').dangerouslySetInnerHTML = data;
+      // console.log(data);
+    })
   }
 
   return (
-       <div>
-          <div className="rowDropDown">
-            <DropDown selected={selectedLang} setSelected={setSelectedLang} typeOfOptions={languages}/>
-            <DropDown selected={selectedOperation} setSelected={setSelectedOperation} typeOfOptions={typeOfOperation}/>
-          </div>
-          {/* <Prettify></Prettify> */}
-          <div className="row">
-            <div className="container">
-              <form>
-                  <textarea id="usercode" className="design" rows="30" cols="50" required value={code} onChange={e => setCode(e.target.value)}></textarea>
-              </form>
-              <SyntaxHighlighter language="javascript" style={docco}>
-                  {code}
-              </SyntaxHighlighter>
-            </div>
-            <button onClick={submitCode}>
-              Run Analysis
-            </button>
-            <div className="container">
-              <SyntaxHighlighter language="javascript" style={docco}>
-                  {machineCode}
-              </SyntaxHighlighter>
-            </div>
-            
-          </div>
+    <div>
+      <div className="row1">
+        <DropDown selected={selectedLang} setSelected={setSelectedLang} typeOfOptions={languages}/>
       </div>
+      <div className="row2">
+        <DropDown selected={selectedOperation} setSelected={setSelectedOperation} typeOfOptions={typeOfOperation}/>
+      </div>
+        
+      <div className="rowBox">
+      <button onClick={submitCode} className="button primary">Run Analysis</button>
+        <form>
+          <textarea autocorrect="off" autocapitalize="off" spellcheck="false" tabindex="0" id="usercode" className="design" rows="30" cols="50" required value={code} onChange={e => setCode(e.target.value)}></textarea>
+        </form> 
+        {/* <Button></Button> */}
+      </div>
+      
+    </div>
   );
 }
 
